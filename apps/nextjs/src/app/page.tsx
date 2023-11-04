b43843f28169aa4831dc9b8f2bcda65d08a65724
GG <gg@tesserakt.gg>
2023-11-04 19:37:01 -0400

    update

--- apps/nextjs/src/app/page.tsx
+++ apps/nextjs/src/app/page.tsx
@@ -12,7 +12,7 @@ export const runtime = "edge";
 
 export default function HomePage() {
   return (
-    <main className="flex h-screen flex-col items-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+    <main className="flex h-screen flex-col items-center bg-gradient-to-b text-white">
       <div className="container mt-12 flex flex-col items-center justify-center gap-4 py-8">
         <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
           VoiceAI
