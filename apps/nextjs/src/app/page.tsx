e67970a4331cc6122ecb292e188dbeb5a5c3a2a0
GG <gg@tesserakt.gg>
2023-11-05 10:55:37 -0500

    initial voice list

--- apps/nextjs/src/app/page.tsx
+++ apps/nextjs/src/app/page.tsx
@@ -1,4 +1,4 @@
-// import { Suspense } from "react";
+import { Suspense } from "react";
 
 import { auth } from "@voiceai/auth";
 import { Button } from "@voiceai/ui";
@@ -6,12 +6,13 @@ import { Button } from "@voiceai/ui";
 import { ChatWindow } from "./_components/ai/chat/chat-window";
 import SloganGenerator from "./_components/ai/completion";
 import { AuthShowcase } from "./_components/auth-showcase";
-
 // import {
 //   CreatePostForm,
 //   PostCardSkeleton,
 //   PostList,
 // } from "./_components/posts";
+
+import { VoiceList } from "./_components/voices/list";
 
 export const runtime = "edge";
 
@@ -31,16 +32,25 @@ export default async function HomePage() {
         {/* <SloganGenerator /> */}
 
         {session && (
-          <div className="w-full ">
-            <ChatWindow
-              endpoint="api/chat"
-              emptyStateComponent={<>empty</>}
-              placeholder="Hello, how can I help you today?"
-              titleText="VoiceAI"
-              emoji="🔊"
-              // showIntermediateStepsToggle={true}
-            ></ChatWindow>
-          </div>
+          <>
+            <div className="w-full ">
+              <ChatWindow
+                endpoint="api/chat"
+                emptyStateComponent={<>empty</>}
+                placeholder="Hello, how can I help you today?"
+                titleText="VoiceAI"
+                emoji="🔊"
+                // showIntermediateStepsToggle={true}
+              ></ChatWindow>
+            </div>
+            <Suspense
+              fallback={
+                <div className="flex w-full flex-col gap-4">LOADING</div>
+              }
+            >
+              <VoiceList />
+            </Suspense>
+          </>
         )}
 
         {/* <CreatePostForm />
