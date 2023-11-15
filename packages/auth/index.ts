7d460b2b7234f1800c62026aa9adb7ba66c0c6fb
GG <gg@tesserakt.gg>
2023-11-15 07:45:47 -0500

    remove new user page

--- packages/auth/index.ts
+++ packages/auth/index.ts
@@ -36,8 +36,8 @@ export const {
     signIn: "/signin",
     signOut: "/signout",
     error: "/auth/error",
-    verifyRequest: "/auth/verify-request",
-    newUser: "/auth/new-user",
+    // verifyRequest: "/auth/verify-request",
+    // newUser: "/auth/new-user",
   },
   providers: [
     Discord,
