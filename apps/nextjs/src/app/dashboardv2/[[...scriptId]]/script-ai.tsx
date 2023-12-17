1c4056d6274c34afc4abcb820bd1bf855d30feb4
GG <gg@tesserakt.gg>
2023-12-17 00:34:23 -0500

    try just blank

--- apps/nextjs/src/app/dashboardv2/[[...scriptId]]/script-ai.tsx
+++ apps/nextjs/src/app/dashboardv2/[[...scriptId]]/script-ai.tsx
@@ -118,127 +118,127 @@ export function ScriptAI({}) {
         </div>
       </div>
       <Separator />
-      {scripts?.length === 0 ? (
+      {/* {scripts?.length === 0 ? (
         <ScriptsEmptyPlaceholder />
-      ) : (
-        <Tabs defaultValue="complete" className="flex-1">
-          <div className="container h-full py-6">
-            <div className="grid h-full items-stretch gap-6 md:grid-cols-[1fr_200px]">
-              <div className="hidden flex-col space-y-4 sm:flex md:order-2">
-                <div className="grid gap-2">
-                  <HoverCard openDelay={200}>
-                    <HoverCardTrigger asChild>
-                      <span className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
-                        Mode
-                      </span>
-                    </HoverCardTrigger>
-                    <HoverCardContent className="w-[320px] text-sm" side="left">
-                      Choose the interface that best suits your task. You can
-                      provide: a simple prompt to complete, starting and ending
-                      text to insert a completion within, or some text with
-                      instructions to edit it.
-                    </HoverCardContent>
-                  </HoverCard>
-                  <TabsList className="grid grid-cols-2">
-                    <TabsTrigger value="complete">
-                      <span className="sr-only">Complete</span>
-                      <Icons.complete className="h-5 w-5" />
-                    </TabsTrigger>
-                    <TabsTrigger value="insert">
-                      <span className="sr-only">Insert</span>
-                      <Icons.insert className="h-5 w-5" />
-                    </TabsTrigger>
-                  </TabsList>
-                </div>
-                <ModelSelector
-                  types={types}
-                  models={models}
-                  onModelSelect={setSelectedModel}
-                />
-                <SimilaritySelector
-                  value={similarity}
-                  onValueChange={setSimilarity}
-                />
-                {/* <MaxLengthSelector defaultValue={[256]} /> */}
-                <StabilitySelector
-                  value={stability}
-                  onValueChange={setStability}
-                />
-                <Button
-                  disabled={!selectedModel || !script}
-                  onClick={async () => {
-                    setLoading(true);
-                    try {
-                      await generateVoice({
-                        // @ts-expect-error need to type this in the state
-                        voice_id: selectedModel?.id,
-                        message: script,
-                        stability: stability?.[0],
-                        similarity: similarity?.[0],
-                      });
-                      setLoading(false);
-                    } catch {}
-                  }}
-                >
-                  {loading ? (
-                    <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
-                  ) : (
-                    "Generate"
-                  )}
-                </Button>
+      ) : ( */}
+      <Tabs defaultValue="complete" className="flex-1">
+        <div className="container h-full py-6">
+          <div className="grid h-full items-stretch gap-6 md:grid-cols-[1fr_200px]">
+            <div className="hidden flex-col space-y-4 sm:flex md:order-2">
+              <div className="grid gap-2">
+                <HoverCard openDelay={200}>
+                  <HoverCardTrigger asChild>
+                    <span className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
+                      Mode
+                    </span>
+                  </HoverCardTrigger>
+                  <HoverCardContent className="w-[320px] text-sm" side="left">
+                    Choose the interface that best suits your task. You can
+                    provide: a simple prompt to complete, starting and ending
+                    text to insert a completion within, or some text with
+                    instructions to edit it.
+                  </HoverCardContent>
+                </HoverCard>
+                <TabsList className="grid grid-cols-2">
+                  <TabsTrigger value="complete">
+                    <span className="sr-only">Complete</span>
+                    <Icons.complete className="h-5 w-5" />
+                  </TabsTrigger>
+                  <TabsTrigger value="insert">
+                    <span className="sr-only">Insert</span>
+                    <Icons.insert className="h-5 w-5" />
+                  </TabsTrigger>
+                </TabsList>
               </div>
-              <div className="md:order-1">
-                <TabsContent value="complete" className="mt-0 border-0 p-0">
-                  <div className="flex h-full flex-col space-y-4">
+              <ModelSelector
+                types={types}
+                models={models}
+                onModelSelect={setSelectedModel}
+              />
+              <SimilaritySelector
+                value={similarity}
+                onValueChange={setSimilarity}
+              />
+              {/* <MaxLengthSelector defaultValue={[256]} /> */}
+              <StabilitySelector
+                value={stability}
+                onValueChange={setStability}
+              />
+              <Button
+                disabled={!selectedModel || !script}
+                onClick={async () => {
+                  setLoading(true);
+                  try {
+                    await generateVoice({
+                      // @ts-expect-error need to type this in the state
+                      voice_id: selectedModel?.id,
+                      message: script,
+                      stability: stability?.[0],
+                      similarity: similarity?.[0],
+                    });
+                    setLoading(false);
+                  } catch {}
+                }}
+              >
+                {loading ? (
+                  <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
+                ) : (
+                  "Generate"
+                )}
+              </Button>
+            </div>
+            <div className="md:order-1">
+              <TabsContent value="complete" className="mt-0 border-0 p-0">
+                <div className="flex h-full flex-col space-y-4">
+                  <Textarea
+                    value={script}
+                    onChange={(e) => setScript(e.target.value)}
+                    placeholder="Write your script here. Then on the right choose a voice model, and click generate. This is a protopye frontend for feedback, I haven't hooked up all the api calls yet. But I think an UI like this may look more professional and helps us have more features in a more extensible way. The top nav has a lot of menu options, I added examples but we can add functionality there as quick action items for people to do with their work. switching the mode activates the grammar/spell checker that can revise your script. I can add colors and logos to the site but wanted to get usability feedback before i go too deep into making it live"
+                    className="min-h-[400px] flex-1 p-4 md:min-h-[700px] lg:min-h-[700px]"
+                  />
+                </div>
+              </TabsContent>
+              <TabsContent value="insert" className="mt-0 border-0 p-0">
+                <div className="flex flex-col space-y-4">
+                  <div className="grid h-full grid-rows-2 gap-6 lg:grid-cols-2 lg:grid-rows-1">
                     <Textarea
                       value={script}
                       onChange={(e) => setScript(e.target.value)}
-                      placeholder="Write your script here. Then on the right choose a voice model, and click generate. This is a protopye frontend for feedback, I haven't hooked up all the api calls yet. But I think an UI like this may look more professional and helps us have more features in a more extensible way. The top nav has a lot of menu options, I added examples but we can add functionality there as quick action items for people to do with their work. switching the mode activates the grammar/spell checker that can revise your script. I can add colors and logos to the site but wanted to get usability feedback before i go too deep into making it live"
-                      className="min-h-[400px] flex-1 p-4 md:min-h-[700px] lg:min-h-[700px]"
+                      placeholder="Your script would go here"
+                      className="h-full min-h-[300px] lg:min-h-[700px] xl:min-h-[700px]"
                     />
-                  </div>
-                </TabsContent>
-                <TabsContent value="insert" className="mt-0 border-0 p-0">
-                  <div className="flex flex-col space-y-4">
-                    <div className="grid h-full grid-rows-2 gap-6 lg:grid-cols-2 lg:grid-rows-1">
+
+                    {revisedScript.length > 0 ? (
                       <Textarea
-                        value={script}
-                        onChange={(e) => setScript(e.target.value)}
-                        placeholder="Your script would go here"
+                        value={revisedScript}
+                        onClick={() => {}}
+                        placeholder="Your revised script"
                         className="h-full min-h-[300px] lg:min-h-[700px] xl:min-h-[700px]"
                       />
+                    ) : (
+                      <div className="cursor-pointer rounded-md border bg-muted text-center">
+                        <span className="">
+                          Your AI Coach would revise your script here
+                        </span>
+                        <Button
+                          onClick={() => {
+                            setLoading(true);
 
-                      {revisedScript.length > 0 ? (
-                        <Textarea
-                          value={revisedScript}
-                          onClick={() => {}}
-                          placeholder="Your revised script"
-                          className="h-full min-h-[300px] lg:min-h-[700px] xl:min-h-[700px]"
-                        />
-                      ) : (
-                        <div className="cursor-pointer rounded-md border bg-muted text-center">
-                          <span className="">
-                            Your AI Coach would revise your script here
-                          </span>
-                          <Button
-                            onClick={() => {
-                              setLoading(true);
-
-                              checkAndPublish(script);
-                            }}
-                          >
-                            Revise Script
-                          </Button>
-                        </div>
-                      )}
-                    </div>
+                            checkAndPublish(script);
+                          }}
+                        >
+                          Revise Script
+                        </Button>
+                      </div>
+                    )}
                   </div>
-                </TabsContent>
-              </div>
+                </div>
+              </TabsContent>
             </div>
           </div>
-        </Tabs>
-      )}
+        </div>
+      </Tabs>
+      {/* )} */}
     </div>
   );
 }
