b63baea51db0e8355cdfe516c5aef2351e0f02c6
GG <gg@tesserakt.gg>
2024-01-02 09:06:55 -0500

    remove unused button

--- apps/nextjs/src/app/(site)/components/chat/prompt-form.tsx
+++ apps/nextjs/src/app/(site)/components/chat/prompt-form.tsx
@@ -47,7 +47,7 @@ export function PromptForm({
       ref={formRef}
     >
       <div className="relative flex max-h-60 w-full grow flex-col overflow-hidden bg-background px-8 sm:rounded-md sm:border sm:px-12">
-        <Tooltip>
+        {/* <Tooltip>
           <TooltipTrigger asChild>
             <button
               onClick={(e) => {
@@ -65,7 +65,7 @@ export function PromptForm({
             </button>
           </TooltipTrigger>
           <TooltipContent>New Chat</TooltipContent>
-        </Tooltip>
+        </Tooltip> */}
         <Textarea
           ref={inputRef}
           tabIndex={0}
