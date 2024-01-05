3d47e4eff243e9a0fc993b0da9fc90fc1985d3bd
GG <gg@tesserakt.gg>
2024-01-05 15:48:52 -0500

    log error

--- packages/api/src/router/voice.ts
+++ packages/api/src/router/voice.ts
@@ -52,88 +52,93 @@ export const voiceRouter = createTRPCRouter({
       }),
     )
     .mutation(async ({ ctx, input }) => {
-      const subscription = await ctx.db.query.subscriptions.findFirst({
-        where: eq(schema.subscriptions.userId, ctx.session.user.id),
-      });
-
-      const isFreePlanGated =
-        input.message &&
-        input.message?.length > 1200 &&
-        subscription?.status !== "ACTIVE";
-
-      if (isFreePlanGated) {
-        throw new TRPCError({
-          code: "FORBIDDEN",
-          message: "Subscribe to a plan to generate more than 1200 characters.",
+      try {
+        const subscription = await ctx.db.query.subscriptions.findFirst({
+          where: eq(schema.subscriptions.userId, ctx.session.user.id),
         });
-      }
 
-      const voice = await ctx.db.query.voices.findFirst({
-        where: eq(schema.voices.id, input.voice_id),
-      });
-      let message = input.message;
+        const isFreePlanGated =
+          input.message &&
+          input.message?.length > 1200 &&
+          subscription?.status !== "ACTIVE";
 
-      if (subscription?.status !== "ACTIVE") {
-        message = addWatermark(message);
-      }
+        if (isFreePlanGated) {
+          throw new TRPCError({
+            code: "FORBIDDEN",
+            message:
+              "Subscribe to a plan to generate more than 1200 characters.",
+          });
+        }
 
-      if (voice?.type === "11LABS") {
-        const payload = {
-          model_id: "eleven_multilingual_v2",
-          text: message,
-          voice_settings: {
-            similarity_boost: input.similarity,
-            stability: input.stability,
-            // style: 0.5,
-            // use_speaker_boost: true,
-          },
-        };
-        const options = {
-          method: "POST",
-          headers: {
-            "xi-api-key": process.env.INTEGRATION_11LABS_API_KEY ?? "",
-            "Content-Type": "application/json",
-            Accept: "audio/mpeg",
-          },
-          responseType: "arraybuffer",
-          body: JSON.stringify(payload),
-        };
+        const voice = await ctx.db.query.voices.findFirst({
+          where: eq(schema.voices.id, input.voice_id),
+        });
+        let message = input.message;
 
-        const response = await fetch(
-          `https://api.elevenlabs.io/v1/text-to-speech/${voice.external_id}`,
-          options,
-        );
+        if (subscription?.status !== "ACTIVE") {
+          message = addWatermark(message);
+        }
 
-        const audioBase64 = Buffer.from(await response.arrayBuffer()).toString(
-          "base64",
-        );
-        const credits = await ctx.db
-          .select({ credits: schema.credits.credits })
-          .from(schema.credits)
-          .where(eq(schema.credits.userId, ctx.session.user.id));
+        if (voice?.type === "11LABS") {
+          const payload = {
+            model_id: "eleven_multilingual_v2",
+            text: message,
+            voice_settings: {
+              similarity_boost: input.similarity,
+              stability: input.stability,
+              // style: 0.5,
+              // use_speaker_boost: true,
+            },
+          };
+          const options = {
+            method: "POST",
+            headers: {
+              "xi-api-key": process.env.INTEGRATION_11LABS_API_KEY ?? "",
+              "Content-Type": "application/json",
+              Accept: "audio/mpeg",
+            },
+            responseType: "arraybuffer",
+            body: JSON.stringify(payload),
+          };
 
-        // TODO: check if user has enough credits
+          const response = await fetch(
+            `https://api.elevenlabs.io/v1/text-to-speech/${voice.external_id}`,
+            options,
+          );
 
-        const generationId = await ctx.db
-          .insert(schema.generations)
-          .values({
+          const audioBase64 = Buffer.from(
+            await response.arrayBuffer(),
+          ).toString("base64");
+          const credits = await ctx.db
+            .select({ credits: schema.credits.credits })
+            .from(schema.credits)
+            .where(eq(schema.credits.userId, ctx.session.user.id));
+
+          // TODO: check if user has enough credits
+
+          const generationId = await ctx.db
+            .insert(schema.generations)
+            .values({
+              userId: ctx.session.user.id,
+              type: "11LABS",
+              prompt: input.message,
+              response: audioBase64,
+              metadata: payload,
+            })
+            .returning({ generationId: schema.generations.id })
+            .then((res) => res?.[0]?.generationId);
+          if (!generationId) throw new Error("Error creating voice");
+          await ctx.db.insert(schema.credits).values({
             userId: ctx.session.user.id,
+            generationId: generationId,
             type: "11LABS",
-            prompt: input.message,
-            response: audioBase64,
-            metadata: payload,
-          })
-          .returning({ generationId: schema.generations.id })
-          .then((res) => res?.[0]?.generationId);
-        if (!generationId) throw new Error("Error creating voice");
-        await ctx.db.insert(schema.credits).values({
-          userId: ctx.session.user.id,
-          generationId: generationId,
-          type: "11LABS",
-          credits: input.message.length,
-        });
+            credits: input.message.length,
+          });
 
-        return { audio: audioBase64 };
+          return { audio: audioBase64 };
+        }
+      } catch (e) {
+        console.log("There was an error", e);
       }
     }),
 });
