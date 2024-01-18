3a1c04096e10bcf5e2ec6bbd47d0f1761a91cc20
GG <gg@tesserakt.gg>
2024-01-18 18:29:35 -0500

    add more height to menu

--- apps/nextjs/src/app/(site)/components/menu.tsx
+++ apps/nextjs/src/app/(site)/components/menu.tsx
@@ -24,7 +24,7 @@ export function Menu({ signOut }: { signOut: () => Promise<null> }) {
   const { theme, setTheme } = useTheme();
 
   return (
-    <Menubar className="sticky top-0 z-50 rounded-none border-b border-none bg-primary px-2 text-destructive-foreground lg:px-4">
+    <Menubar className="sticky top-0 z-50 h-12 rounded-none border-b border-none bg-primary px-2 text-destructive-foreground lg:px-4">
       <MenubarMenu>
         <MenubarTrigger className="font-bold">Script Timer</MenubarTrigger>
         <MenubarContent>
