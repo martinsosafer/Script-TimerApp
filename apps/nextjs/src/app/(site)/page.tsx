5e58c2fe6aa861230232019d20a1bc313a46c222
GG <gg@tesserakt.gg>
2024-01-25 16:53:36 -0500

    fix spacing

--- apps/nextjs/src/app/(site)/page.tsx
+++ apps/nextjs/src/app/(site)/page.tsx
@@ -32,8 +32,8 @@ export default function LandingPage() {
               />
             </AspectRatio>
 
-            <div className="space-x-4">
-              <p className="inline-flex font-bold text-primary">Skip to:</p>
+            <div className="flex flex-col space-x-4 space-y-4 md:flex-row">
+              <p className="my-auto font-bold text-primary">Skip to:</p>
               <a
                 target="_blank"
                 className="inline-flex h-9 items-center justify-center rounded-md bg-primary px-4 py-2 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
