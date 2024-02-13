73a5bb77ae1972a110c8d2325c7b066039488e06
martinsosafer <martin@tesserakt.gg>
2024-02-13 14:55:24 -0300

    changes

--- apps/nextjs/src/app/(site)/components/chat/actorsdropdown.tsx
+++ apps/nextjs/src/app/(site)/components/chat/actorsdropdown.tsx
@@ -55,7 +55,7 @@ export function ActorsDropdown({
             voices={voices}
             setSelectedModel={setSelectedModel}
           />
-          <TooltipContent>Choose your voice model</TooltipContent>
+          <TooltipContent>Choose voice model</TooltipContent>
         </TooltipTrigger>
       </Tooltip>
     </div>
