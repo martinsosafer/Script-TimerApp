0e833cbfc0fd7c4a1ae0c96ca8290eab347c7c23
martinsosafer <martin@tesserakt.gg>
2024-02-22 13:11:03 -0300

    prompt chat

--- apps/nextjs/src/app/api/chat/route.ts
+++ apps/nextjs/src/app/api/chat/route.ts
@@ -3,7 +3,9 @@ import { OpenAIStream, StreamingTextResponse } from "ai";
 import OpenAI from "openai";
 
 import { auth } from "@voiceai/auth";
+import { db } from "@voiceai/db";
 
+import { prompts } from "~/app/(site)/data/prompts";
 import { nanoid } from "~/utils/helpers";
 
 // export const runtime = "edge";
@@ -12,60 +14,81 @@ const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
 export async function POST(req: Request) {
-  const json = await req.json();
-  const { messages, previewToken } = json;
-  const userId = (await auth())?.user.id;
+  try {
+    const session = await auth();
 
-  if (!userId) {
-    return new Response("Unauthorized", {
-      status: 401,
+    if (!session?.user) {
+      return new Response("Unauthorized", {
+        status: 401,
+      });
+    }
+
+    const body = await req.json();
+
+    let systemMessage = "";
+    if (body?.prompt) {
+      const promptContent = prompts.find(
+        (prompt) => prompt.id === body?.prompt,
+      );
+
+      systemMessage = promptContent?.prompt_ai ?? "";
+    }
+
+    const { messages, previewToken } = body;
+    const userId = session.user.id;
+
+    if (previewToken) {
+      openai.apiKey = previewToken;
+    }
+
+    const res = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      messages,
+      temperature: 0.7,
+      stream: true,
     });
+
+    const stream = OpenAIStream(res, {
+      async onCompletion(completion) {
+        try {
+          const title = messages[0].content.substring(0, 100);
+          const id = body.id ?? nanoid();
+          const createdAt = Date.now();
+          const path = `/chat/${id}`;
+          const payload = {
+            id,
+            title,
+            userId,
+            createdAt,
+            path,
+            messages: [
+              ...messages,
+              {
+                content: systemMessage, // Include system message here
+                role: "system", // Assuming system messages have a different role
+              },
+              {
+                content: completion,
+                role: "assistant",
+              },
+            ],
+          };
+          await kv.hmset(`chat:${id}`, payload);
+          await kv.zadd(`user:chat:${userId}`, {
+            score: createdAt,
+            member: `chat:${id}`,
+          });
+        } catch (error) {
+          console.error("Error processing completion:", error);
+        }
+      },
+    });
+
+    return new StreamingTextResponse(stream);
+  } catch (error) {
+    console.error("Error:", error);
+    return new Response("Internal Server Error", { status: 500 });
   }
-
-  if (previewToken) {
-    openai.apiKey = previewToken;
-  }
-
-  const res = await openai.chat.completions.create({
-    model: "gpt-3.5-turbo",
-    messages,
-    temperature: 0.7,
-    stream: true,
-  });
-
-  const stream = OpenAIStream(res, {
-    async onCompletion(completion) {
-      try {
-        const title = json.messages[0].content.substring(0, 100);
-        const id = json.id ?? nanoid();
-        const createdAt = Date.now();
-        const path = `/chat/${id}`;
-        const payload = {
-          id,
-          title,
-          userId,
-          createdAt,
-          path,
-          messages: [
-            ...messages,
-            {
-              content: completion,
-              role: "assistant",
-            },
-          ],
-        };
-        await kv.hmset(`chat:${id}`, payload);
-        await kv.zadd(`user:chat:${userId}`, {
-          score: createdAt,
-          member: `chat:${id}`,
-        });
-      } catch (error) {
-        console.error("Error processing completion:", error);
-      }
-    },
-  });
-
-  return new StreamingTextResponse(stream);
 }
 // // import type { NextRequest } from "next/server";
 // // import { NextResponse } from "next/server";
