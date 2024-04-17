e618b441bf72acc967099668001813441c781a95
martinsosafer <martin@tesserakt.gg>
2024-04-17 13:27:54 -0300

    changes

---
+++ apps/nextjs/src/app/(site)/components/slide-cards.tsx
@@ -0,0 +1,81 @@
+import React from "react";
+import Image from "next/image";
+
+import actorsImg from "../../../../public/actors.jpg";
+import aiwritterImg from "../../../../public/aiwritting.jpg";
+import speechImg from "../../../../public/speech.png";
+
+function SlideCards() {
+  return (
+    <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
+      <div className="beauty-card group relative cursor-pointer items-center justify-center overflow-hidden rounded-md border-2 border-black transition-shadow hover:shadow-xl hover:shadow-black/30">
+        <div className="h-96 w-72">
+          <Image
+            className="h-full w-full object-cover transition-transform duration-500 group-hover:rotate-3 group-hover:scale-125"
+            src={speechImg}
+            alt="speech area image"
+          />
+        </div>
+        <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
+        <div className="absolute inset-0 flex translate-y-[60%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
+          <h1 className="font-dmserif text-3xl font-bold text-white">
+            Script Writing
+          </h1>
+          <p className="mb-3 text-lg italic text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis
+            dolore adipisci placeat.
+          </p>
+          <button className="inline-flex h-9 items-center justify-center rounded-xl bg-sky-500 px-4 py-2 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-blue-600 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300">
+            See More
+          </button>
+        </div>
+      </div>
+      <div className="beauty-card group relative cursor-pointer items-center justify-center overflow-hidden rounded-md border border-black transition-shadow hover:shadow-xl hover:shadow-black/30">
+        <div className="h-96 w-72">
+          <Image
+            className="h-full w-full object-cover transition-transform duration-500 group-hover:rotate-3 group-hover:scale-125"
+            src={actorsImg}
+            alt="voice actor library"
+          />
+        </div>
+        <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
+        <div className="absolute inset-0 flex translate-y-[60%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
+          <h1 className="font-dmserif text-3xl font-bold text-white">
+            Voice Library
+          </h1>
+          <p className="mb-3 text-lg italic text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis
+            dolore adipisci placeat.
+          </p>
+          <button className="inline-flex h-9 items-center justify-center rounded-xl bg-sky-500 px-4 py-2 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-blue-600 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300">
+            See More
+          </button>
+        </div>
+      </div>
+      <div className="beauty-card group relative cursor-pointer items-center justify-center overflow-hidden rounded-md border border-black transition-shadow hover:shadow-xl hover:shadow-black/30">
+        <div className="h-96 w-72">
+          <Image
+            className="h-full w-full object-cover transition-transform duration-500 group-hover:rotate-3 group-hover:scale-125"
+            src={aiwritterImg}
+            alt="Voice Library image"
+          />
+        </div>
+        <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
+        <div className="absolute inset-0 flex translate-y-[60%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
+          <h1 className="font-dmserif text-3xl font-bold text-white">
+            Script Coach AI
+          </h1>
+          <p className="mb-3 text-lg italic text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis
+            dolore adipisci placeat.
+          </p>
+          <button className="inline-flex h-9 items-center justify-center rounded-xl bg-sky-500 px-4 py-2 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-blue-600 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300">
+            See More
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default SlideCards;
--- apps/nextjs/src/app/(site)/success/page.tsx
+++ apps/nextjs/src/app/(site)/success/page.tsx
@@ -1,11 +1,36 @@
+"use client"
 import React from "react";
 
+import { AspectRatio } from "@voiceai/ui/@/components/ui/aspect-ratio";
+
+import SlideCards from "../components/slide-cards";
+import ThanksCard from "../components/thanksCard";
+import Confetti from "react-confetti"
 function SuccessPage() {
   return (
-    <div>
-      <h2>hey thanks for buying </h2>
+     <div className="flex min-h-screen flex-col items-center justify-center space-y-4 text-center mb-20">
+      <Confetti
+        width={window.innerWidth}
+        height={window.innerHeight}
+        numberOfPieces={1000}
+        recycle={false}
+        gravity={0.1}
+        initialVelocityX={2}
+        initialVelocityY={10}
+        colors={["#0123e7", "#eb8806"]}
+      />
+      <ThanksCard />
+      <AspectRatio ratio={30 / 8}>
+        <iframe
+          src="https://player.vimeo.com/video/904618003?h=e72a50f24d&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
+          className="h-full w-full"
+          allow="autoplay; fullscreen; picture-in-picture"
+          title="Script-Timer Ai_ On boarding video (Short version) (1)"
+        />
+      </AspectRatio>
+      <h2 className="text-5xl font-bold text-primary mb-8 tracking-wider">Welcome to Our Site!</h2>
+      <SlideCards />
     </div>
   );
 }
-
 export default SuccessPage;
