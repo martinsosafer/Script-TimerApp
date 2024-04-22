5fcc19867ca36c449ef90f8857502ade8122ce30
martinsosafer <martin@tesserakt.gg>
2024-04-22 15:00:08 -0300

    changes to stripekey

--- apps/nextjs/src/app/api/prices/route.ts
+++ apps/nextjs/src/app/api/prices/route.ts
@@ -2,6 +2,12 @@ import { NextResponse } from "next/server";
 import { Stripe } from "stripe";
 
 export async function GET() {
+  const stripeSecretKey = process.env.STRIPE_SECRET_KEY;
+
+  if (!stripeSecretKey) {
+    return NextResponse.error(new Error("Stripe secret key is not defined."));
+  }
+
   const stripe = new Stripe(stripeSecretKey);
   const products = await stripe.products.list();
 
