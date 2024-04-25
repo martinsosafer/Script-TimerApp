8c8fe64eadebb6ce1b7e3d0f023fd62ed481d784
martinsosafer <martin@tesserakt.gg>
2024-04-25 12:15:12 -0300

    changes to stripe

--- apps/nextjs/src/app/(site)/plans/page.tsx
+++ apps/nextjs/src/app/(site)/plans/page.tsx
@@ -19,10 +19,7 @@ async function loadProducts() {
   const stripe = new Stripe(stripeSecretKey);
   const stripeProducts = await stripe.products.list();
 
-  // Transform Stripe products into the structure expected by PriceCard
   const products = stripeProducts.data.map((stripeProduct) => {
-    // Log metadata for each product
-
     return {
       id: stripeProduct.id,
       name: stripeProduct.name,
@@ -44,25 +41,46 @@ async function loadProducts() {
     };
   });
 
-  return products;
+  products.sort((a, b) => a.metadata.price - b.metadata.price);
+
+  // Filter products into monthly and yearly plans
+  const monthlyPlans = products.filter(
+    (product) => product.metadata.price <= 39,
+  );
+  const yearlyPlans = products.filter((product) => product.metadata.price > 39);
+
+  const orderedMonthlyPlans = monthlyPlans.sort(
+    (a, b) => a.metadata.price - b.metadata.price,
+  );
+
+  const orderedYearlyPlans = yearlyPlans.sort(
+    (a, b) => a.metadata.price - b.metadata.price,
+  );
+
+  return { monthlyPlans: orderedMonthlyPlans, yearlyPlans: orderedYearlyPlans };
 }
 
 async function PlansPage() {
-  const products = await loadProducts();
+  const { monthlyPlans, yearlyPlans } = await loadProducts();
 
   return (
     <>
       <div className="mx-auto max-w-7xl bg-white px-4 pt-10 text-center sm:px-6 lg:px-8">
         <h2 className="  text-8xl font-extrabold text-primary sm:text-5xl sm:leading-tight sm:tracking-tight">
           <span className="block">START NOW WITH</span>
-          <span className="block">SCRIPT COACHING & VOICEOVERS</span>
+          <span className="block">
+            SCRIPT WRITING, VOICEOVERS & MASTERCLASSES
+          </span>
         </h2>
-        <p className="mx-auto mt-4 max-w-3xl text-lg font-bold text-secondary-foreground ">
+        <p className="mx-auto mt-4 max-w-3xl text-lg  font-medium  text-gray-500 ">
           Choose an affordable plan that's packed with the best features for
           engaging your audience, creating scripts, and more.
         </p>
+        <span className="font-bold text-black">
+          Save up to 35% on yearly plans!
+        </span>
       </div>
-      <PriceCard products={products} />
+      <PriceCard monthlyPlans={monthlyPlans} yearlyPlans={yearlyPlans} />
     </>
   );
 }
