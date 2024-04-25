34fc7da9f4dc6ec2f0d96f65bfa56843cb4930ae
martinsosafer <martin@tesserakt.gg>
2024-04-25 15:55:47 -0300

    added Agustin to dashboard

--- apps/nextjs/src/app/(site)/admin/layout.tsx
+++ apps/nextjs/src/app/(site)/admin/layout.tsx
@@ -13,6 +13,7 @@ export default async function AdminLayout({ children }: Props) {
     "b76a1726-e72e-43ca-b250-840af61dfdfe",
     "59d42d74-5b5a-4b52-aedb-8bcb8f076381",
     "a2b976c5-977d-41f3-8152-3ff7eec41f11",
+    "abf2ceef-2864-4506-80ac-c4452023f003",
   ];
 
   if (!session?.user?.id || !allowedIds.includes(session.user.id)) {
