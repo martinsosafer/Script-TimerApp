efffb95061d07af1ea9de0216ec9098172b37be2
AgustinSant <agustinmsant@gmail.com>
2024-05-31 18:00:40 -0300

    resolve merge conflicts

--- packages/ui-library/@/components/ui/icons.tsx
+++ packages/ui-library/@/components/ui/icons.tsx
@@ -1472,14 +1472,13 @@ function IconChevronRight({
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-      className={cn("h-6 w-6", className)}
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className={className}
       {...props}
     >
-      <path d="M7 10v12" />
-      <path d="M15 5.88 14 10h5.83a2 2 0 0 1 1.92 2.56l-2.33 8A2 2 0 0 1 17.5 22H4a2 2 0 0 1-2-2v-8a2 2 0 0 1 2-2h2.76a2 2 0 0 0 1.79-1.11L12 2h0a3.13 3.13 0 0 1 3 3.88Z" />
+      <path d="m9 18 6-6-6-6" />
     </svg>
   );
 }
