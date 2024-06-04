62c8515ad98d6677ea70d26288c95db5b7926fe5
AgustinSant <agustinmsant@gmail.com>
2024-06-04 10:00:08 -0300

    add prifle page

---
+++ apps/nextjs/src/app/(site)/my-profile/page.tsx
@@ -0,0 +1,10 @@
+export default function MyProfile() {
+  return (
+    <div className="flex h-full w-full justify-center bg-[#FAFAFA] py-10">
+      <div className="mt-40 flex h-[800px] w-[1024px] flex-col items-center rounded-xl bg-white shadow-xl shadow-black">
+        {" "}
+        hoplaskdjlaskjd{" "}
+      </div>
+    </div>
+  );
+}
