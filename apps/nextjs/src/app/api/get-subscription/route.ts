6cb6a2beedc89cc14a3293754a307c1babed8e12
AgustinSant <agustinmsant@gmail.com>
2024-06-06 08:50:08 -0300

    fix get-subscription building error

--- apps/nextjs/src/app/api/get-subscription/route.ts
+++ apps/nextjs/src/app/api/get-subscription/route.ts
@@ -5,13 +5,13 @@ export async function GET() {
   const stripeSecretKey = process.env.STRIPE_SECRET_KEY;
 
   if (!stripeSecretKey) {
-    return new Error("Stripe secret key is not defined."));
+    return new Error("Stripe secret key is not defined.");
   }
 
   const stripe = new Stripe(stripeSecretKey);
   const products = await stripe.products.list();
 
-  console.log('STRIPE PRODUCTS', products);
+  console.log("STRIPE PRODUCTS", products);
 
   return NextResponse.json(products.data);
 }
