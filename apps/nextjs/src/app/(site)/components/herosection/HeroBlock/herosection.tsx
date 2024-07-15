92a339971f8550ff42c18f9c899b86ce1ff39c89
martinsosafer <martin@tesserakt.gg>
2024-07-15 17:49:15 -0300

    changes to streaming with qa

--- apps/nextjs/src/app/(site)/components/herosection/HeroBlock/herosection.tsx
+++ apps/nextjs/src/app/(site)/components/herosection/HeroBlock/herosection.tsx
@@ -52,7 +52,7 @@ export default function HeroSection() {
           <MotionTransition className="flex items-start justify-center">
             <div className="w-full">
               <iframe
-                src="https://player.vimeo.com/video/904618003?h=e72a50f24d&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
+                src="https://player.vimeo.com/video/969324308?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
                 className="h-[340px] w-[520px] "
                 allow="autoplay; fullscreen; picture-in-picture"
                 title="Script-Timer Ai_ On boarding video (Short version) (1)"
