a98abb8f8a192e15f0f357f14d5fbc668a606a3a
martinsosafer <martin@tesserakt.gg>
2024-08-07 13:32:40 -0300

    changes to audio

--- apps/nextjs/src/app/(site)/translator/translator-components/AudioTranslate/index.tsx
+++ apps/nextjs/src/app/(site)/translator/translator-components/AudioTranslate/index.tsx
@@ -60,92 +60,102 @@ export default function AudioTranslate({}) {
   };
 
   return (
-    <div>
-      <div className="w-full max-w-xl">
-        <div className="mt-10 flex flex-row items-center space-x-3">
-          <div className="flex h-9 w-9 items-center justify-center gap-3 rounded-lg border-2 border-[#1877F290] bg-blue-300">
-            <IconEar className="text-black" />
-          </div>
-          <div className="">
-            <p className="text-left font-medium">
-              Upload Audio File <span className="text-slate-500 ">)</span>
-            </p>
-          </div>
+    <div className="w-full max-w-xl">
+      <div className="mt-10 flex flex-row items-center space-x-3">
+        <div className="flex h-9 w-9 items-center justify-center gap-3 rounded-lg border-2 border-[#1877F290] bg-blue-300">
+          <IconEar className="text-black" />
         </div>
-
-        <label className="my-1 ml-1 block text-left text-sm font-medium text-gray-900 dark:text-white">
-          Upload file:
-        </label>
-        <input
-          className="mb-2 block w-full cursor-pointer rounded-lg border border-gray-300 bg-gray-50 text-sm text-gray-900 focus:outline-none dark:border-gray-600 dark:bg-gray-700 dark:text-gray-400 dark:placeholder-gray-400"
-          type="file"
-          accept="audio/*"
-          onChange={handleFileChange}
-        />
-        <p className="my-2 text-sm text-gray-500 dark:text-gray-300">
-          The following file formats are accepted: m4a, mp3, webm, mp4, mpga,
-          wav, and mpeg.
-        </p>
-
-        <div className="mb-5 flex items-center space-x-3">
-          <div className="flex h-9 w-9 items-center justify-center gap-3 rounded-lg border-2 border-[#1877F290] bg-blue-300">
-            <IconFlag className="text-black" />
-          </div>
-          <p className="text-left font-medium">Choose your Language.</p>
+        <div className="">
+          <p className="text-left font-medium">
+            Upload Audio File <span className="text-slate-500 ">)</span>
+          </p>
         </div>
-
-        <select
-          className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-black focus:ring-black dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-black dark:focus:ring-black"
-          onChange={handleChange}
-          value={language}
-        >
-          {languages.map((language) => (
-            <option key={language.value} value={language.value}>
-              {language.label}
-            </option>
-          ))}
-        </select>
-
-        {!loading && (
-          <button
-            className="mt-8 w-full rounded-xl bg-primary px-4 py-2 font-medium text-white hover:bg-primary/80 sm:mt-10"
-            onClick={translateAudio}
-          >
-            Translate &rarr;
-          </button>
-        )}
-        {loading && (
-          <button
-            className="mt-8 w-full rounded-xl bg-primary px-4 py-2 font-medium text-white hover:bg-primary/80 sm:mt-10"
-            disabled
-          >
-            <LoadingDots color="white" style="large" />
-          </button>
-        )}
-
-        {generatedTranslation && (
-          <>
-            <label className="text-md my-2 block text-left font-medium text-gray-900 dark:text-white">
-              Translation:
-            </label>
-            <div
-              className="w-full cursor-pointer rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900"
-              onClick={() => {
-                navigator.clipboard.writeText(generatedTranslation);
-                toast({
-                  title: "Translation copied to clipboard",
-                });
-              }}
-            >
-              <p> {generatedTranslation}</p>
-            </div>
-            <p className="my-1 text-sm text-gray-500 dark:text-gray-300">
-              Click on translation to copy on clipboard
-            </p>
-            <div className="mb-[-80px]" />
-          </>
-        )}
       </div>
+
+      <label className="my-1 ml-1 block text-left text-sm font-medium text-gray-900 dark:text-white">
+        Upload file:
+      </label>
+      <input
+        className="mb-2 block w-full cursor-pointer rounded-lg border border-gray-300 bg-gray-50 text-sm text-gray-900 focus:outline-none dark:border-gray-600 dark:bg-gray-700 dark:text-gray-400 dark:placeholder-gray-400"
+        type="file"
+        accept="audio/*"
+        onChange={handleFileChange}
+      />
+      <p className="my-2 text-sm text-gray-500 dark:text-gray-300">
+        The following file formats are accepted: m4a, mp3, webm, mp4, mpga, wav,
+        and mpeg.
+      </p>
+
+      <div className="mb-5 flex items-center space-x-3">
+        <div className="flex h-9 w-9 items-center justify-center gap-3 rounded-lg border-2 border-[#1877F290] bg-blue-300">
+          <IconFlag className="text-black" />
+        </div>
+        <p className="text-left font-medium">Choose your Language.</p>
+      </div>
+
+      <select
+        className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-black focus:ring-black dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-black dark:focus:ring-black"
+        onChange={handleChange}
+        value={language}
+      >
+        {languages.map((language) => (
+          <option key={language.value} value={language.value}>
+            {language.label}
+          </option>
+        ))}
+      </select>
+
+      {!loading && (
+        <button
+          className="mt-8 w-full rounded-xl bg-primary px-4 py-2 font-medium text-white hover:bg-primary/80 sm:mt-10"
+          onClick={translateAudio}
+        >
+          Translate &rarr;
+        </button>
+      )}
+      {loading && (
+        <button
+          className="mt-8 w-full rounded-xl bg-primary px-4 py-2 font-medium text-white hover:bg-primary/80 sm:mt-10"
+          disabled
+        >
+          <LoadingDots color="white" style="large" />
+        </button>
+      )}
+
+      {generatedTranslation && (
+        <>
+          <label className="text-md my-2 block text-left font-medium text-gray-900 dark:text-white">
+            Translation:
+          </label>
+          <p className="my-1 text-sm text-gray-500 dark:text-gray-300">
+            Click on translation to copy on clipboard.
+          </p>
+          <div
+            className="w-full cursor-pointer rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900"
+            onClick={() => {
+              navigator.clipboard.writeText(generatedTranslation);
+              toast({
+                title: "Translation copied to clipboard",
+              });
+            }}
+          >
+            <p> {generatedTranslation}</p>
+          </div>
+          <p className="my-1 text-sm text-gray-500 dark:text-gray-300">
+            Click on translation to copy on clipboard
+          </p>{" "}
+          <button
+            className="my-2 text-sm text-blue-500 underline"
+            onClick={() => {
+              navigator.clipboard.writeText(generatedTranslation);
+              window.location.href = "/texttovoice";
+            }}
+          >
+            Copy and go to Text to Voice
+          </button>
+          <div className="mb-[-80px]" />
+        </>
+      )}
     </div>
   );
 }
