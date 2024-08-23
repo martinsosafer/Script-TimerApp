16a2805cf2718f1644a64eb044c2329aa7c976c5
martinsosafer <martin@tesserakt.gg>
2024-08-23 10:40:36 -0300

    clone voice apikey change

--- packages/api/src/router/voiceCustom.ts
+++ packages/api/src/router/voiceCustom.ts
@@ -105,7 +105,7 @@ export const voiceCustomRouter = createTRPCRouter({
           {
             method: "POST",
             headers: {
-              "xi-api-key": process.env.INTEGRATION_11LABS_API_KEY ?? "",
+              "xi-api-key": process.env.CLONE_11LABS_API_KEY ?? "",
             },
             body: form,
           },
