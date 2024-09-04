4c81cf5b05485254dced16b6daefb1043d7f5687
martinsosafer <martin@tesserakt.gg>
2024-09-04 10:54:47 -0300

    auth problem fix

--- packages/auth/index.ts
+++ packages/auth/index.ts
@@ -1,6 +1,5 @@
 /* eslint-disable @typescript-eslint/unbound-method */
 /* @see https://github.com/nextauthjs/next-auth/pull/8932 */
-"use client";
 
 import CredentialsProviders from "@auth/core/providers/credentials";
 import Google from "@auth/core/providers/google";
