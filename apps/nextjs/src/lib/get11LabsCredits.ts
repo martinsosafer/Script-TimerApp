8c7e648328ae410e31c1a5d691c2aae47717854d
martinsosafer <martin@tesserakt.gg>
2024-09-06 13:23:39 -0300

    bad thing

--- apps/nextjs/src/lib/get11LabsCredits.ts
+++ apps/nextjs/src/lib/get11LabsCredits.ts
@@ -1,6 +1,6 @@
 export async function fetchUserCredits(userId: string): Promise<number> {
   try {
-    const baseUrl = "http://localhost:3000";
+    const baseUrl = process.env.NEXT_PUBLIC_HOST_URL;
     const response = await fetch(
       `${baseUrl}/api/get11LabsCredits?userId=${userId}`,
     );
