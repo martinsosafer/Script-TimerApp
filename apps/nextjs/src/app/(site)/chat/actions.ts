7b9f4dc660ecbc0b710a78c20d923214b51b419a
AgustinSant <agustinmsant@gmail.com>
2024-09-26 09:50:13 -0300

    remove throw error con credits retrieve action for chat page

--- apps/nextjs/src/app/(site)/chat/actions.ts
+++ apps/nextjs/src/app/(site)/chat/actions.ts
@@ -4,9 +4,6 @@ import { db } from "@voiceai/db";
 
 export default async function getOpenAiCredits(userId: string) {
   try {
-    if (!userId) {
-      throw new Error("User ID is required.");
-    }
     const openAiCredits = await db.query.openAiCredit.findFirst({
       where: (openAiCredit, { eq }) => eq(openAiCredit.userId, userId),
     });
