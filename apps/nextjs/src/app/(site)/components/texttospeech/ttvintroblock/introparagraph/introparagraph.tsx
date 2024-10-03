e68c3f3acf3536f8b6776df2ef726aa7cc4712e6
martinsosafer <martin@tesserakt.gg>
2024-10-03 08:44:43 -0300

    changes

--- apps/nextjs/src/app/(site)/components/texttospeech/ttvintroblock/introparagraph/introparagraph.tsx
+++ apps/nextjs/src/app/(site)/components/texttospeech/ttvintroblock/introparagraph/introparagraph.tsx
@@ -5,13 +5,18 @@ interface IntroParagraphProps {
   status: string | undefined;
   credits: number;
 }
-
 const characters: Record<string, string> = {
   FREE: "500 characters",
   FREE_TRIAL: "1000 characters",
   STUDENT: "2000 characters",
   CREATOR: "5000 characters",
   BUSINESS: "10000 characters",
+  STUDENTCLMO: "2000 characters",
+  CREATORCLMO: "5000 characters",
+  BUSINESSCLMO: "10000 characters",
+  STUDENTCLYR: "2000 characters",
+  CREATORCLYR: "5000 characters",
+  BUSINESSCLYR: "10000 characters",
 };
 
 // Function to get total credits based on the subscription status
@@ -20,20 +25,28 @@ const getTotalCredits = (status: string | undefined): number => {
 
   switch (status) {
     case "FREE":
-      return 10000;
     case "FREE_TRIAL":
       return 10000;
+
     case "STUDENT":
+    case "STUDENTCLMO":
+    case "STUDENTCLYR":
       return 40000;
+
     case "CREATOR":
+    case "CREATORCLMO":
+    case "CREATORCLYR":
       return 80000;
+
     case "BUSINESS":
+    case "BUSINESSCLMO":
+    case "BUSINESSCLYR":
       return 125000;
+
     default:
       return 0;
   }
 };
-
 function IntroParagraph(props: IntroParagraphProps) {
   const { status, credits } = props;
   const totalCredits = getTotalCredits(status);
