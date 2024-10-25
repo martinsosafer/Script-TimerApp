12b9b1558bc4962ca832a1bdcbd8f6a407142126
AgustinSant <agustinmsant@gmail.com>
2024-10-25 10:12:49 -0300

    update nav items

--- apps/nextjs/src/app/(site)/components/navbar/profile-nav-item/index.tsx
+++ apps/nextjs/src/app/(site)/components/navbar/profile-nav-item/index.tsx
@@ -47,7 +47,7 @@ export default function ProfileNavItem({
                   </li>
                   <li>
                     <Link
-                      href={"/plans"}
+                      href={"/new-plans"}
                       className="-mx-2 block rounded-lg p-2 font-semibold text-gray-800 transition duration-300 ease-in-out hover:bg-gradient-to-br hover:from-indigo-200 hover:via-blue-200 hover:to-orange-200 hover:text-indigo-600 dark:text-secondary-foreground dark:hover:bg-gradient-to-br dark:hover:from-gray-800 dark:hover:via-gray-700 dark:hover:to-gray-600 dark:hover:text-indigo-600"
                     >
                       <div className="flex items-center gap-1">
