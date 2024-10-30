c4fd575ab8e9c114e2b4d685c3c88e0f79f29b47
martinsosafer <martin@tesserakt.gg>
2024-10-30 16:27:55 -0300

    changes to modals

--- apps/nextjs/src/app/(site)/components/home/index.tsx
+++ apps/nextjs/src/app/(site)/components/home/index.tsx
@@ -20,7 +20,7 @@ export default function Home({
   trialExpiration: boolean;
   session: SubscriptionData | null | undefined;
 }) {
-  const [openTrialModal, setOpenTrialModal] = useState(true);
+  const [openTrialModal, setOpenTrialModal] = useState(trialExpiration);
   const [openModal, setOpenModal] = useState(false);
 
   useEffect(() => {
