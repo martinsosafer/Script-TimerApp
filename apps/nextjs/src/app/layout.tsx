d3c569bf0df3832d778da935e9a14facae7e5af9
martinsosafer <martin@tesserakt.gg>
2024-11-14 07:48:16 -0300

    voice mockup

--- apps/nextjs/src/app/layout.tsx
+++ apps/nextjs/src/app/layout.tsx
@@ -66,15 +66,8 @@ export default function RootLayout({
           "h-screen bg-background",
         ].join(" ")}
       >
-<<<<<<< HEAD
-        <GoogleAnalytics />
         {children}
-        <HotJar />
-        <PageAnalytics />
-=======
-        {props.children}
         <Squid />
->>>>>>> 6cfcc09fb3e1630a9180181ff4eb2aa978c23723
       </body>
     </html>
   );
