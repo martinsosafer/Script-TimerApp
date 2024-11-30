30967004fdee654a8cc039eec9dd25ef7ff92d8b
AgustinSant <agustinmsant@gmail.com>
2024-11-30 13:50:00 -0300

    update special visibility

--- apps/nextjs/src/app/(site)/layout.tsx
+++ apps/nextjs/src/app/(site)/layout.tsx
@@ -59,7 +59,10 @@ export default async function Layout(props: { children: React.ReactNode }) {
           signIn={signInServer}
           session={session}
         />
-        <MonthlySpecialProvider monthlySpecials={monthlySpecials}>
+        <MonthlySpecialProvider
+          monthlySpecials={monthlySpecials}
+          session={session}
+        >
           <div>{props.children}</div>
         </MonthlySpecialProvider>
 
