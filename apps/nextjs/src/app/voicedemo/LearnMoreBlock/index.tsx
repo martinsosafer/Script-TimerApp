b7217b039dc5f4f05672934081dc18d8186dbbf1
martinsosafer <martin@tesserakt.gg>
2024-12-02 11:59:05 -0300

    change it

--- apps/nextjs/src/app/voicedemo/LearnMoreBlock/index.tsx
+++ apps/nextjs/src/app/voicedemo/LearnMoreBlock/index.tsx
@@ -56,28 +56,28 @@ export default function LearnMoreBlock() {
     {
       title: "Pre-Production & Story",
       description:
-        "Get ideas and start production faster. Manage your pre-production workflow and use AI to help to write a script/story in minutes.",
+        "Craft videos with ease and precision, ensure your brand story drives desired results. Go from idea to script to voice to storyboard in minutes.",
       image: "/LearnMoreImg1.png",
       imageAlt: "Pre-production and story development visualization",
     },
     {
       title: "Social Media Creation",
       description:
-        "Build viral social media marketing campaigns with the right focus points. Connect and share.",
+        "Blend education and entertainment effortlessly. Keep viewers engaged while educating them with the right hooks, posts, content and videos.",
       image: "/LearnMoreImg2.png",
       imageAlt: "Social media content creation visualization",
     },
     {
       title: "Marketing and Leads",
       description:
-        "Get ahead of the competition by creating viral marketing campaigns that will get you more leads.",
+        "Get ahead of the competition by creating MQL and SQL and adding value with content that appeals to your particular audience.",
       image: "/LearnMoreImg3.png",
       imageAlt: "Marketing and leads visualization",
     },
     {
       title: "Masterclass Content",
       description:
-        "Take notes by AI powered transcription and get ahead of your competition by learning from the best.",
+        "Your learning is delivered by instructors seen at Harvard, Microsoft, Cisco and hundreds of others. Improve your career with knowledge.",
       image: "/LearnMoreImg4.png",
       imageAlt: "Masterclass content visualization",
     },
