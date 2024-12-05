d7cc1f8e6b9f5c8a2d29821fcbd6a566606896e1
AgustinSant <agustinmsant@gmail.com>
2024-12-05 07:54:02 -0300

    add awards block

--- apps/nextjs/src/app/lp/sections/awards-block/index.tsx
+++ apps/nextjs/src/app/lp/sections/awards-block/index.tsx
@@ -3,42 +3,38 @@ import AwardImg1 from "@public/AwardsBlockImg1.png";
 import AwardImg2 from "@public/AwardsBlockImg2.png";
 import AwardImg3 from "@public/AwardsBlockImg3.png";
 
-import { poppins } from "~/app/fonts";
+import { poppins, roboto } from "~/app/fonts";
 
 export default function AwardsBlock() {
   return (
-    <div className={`h-full w-full bg-[#E2E8F0]  ${poppins.className}`}>
-      <div className="container mx-auto items-center pb-[24px]  text-center">
-        <h2 className="mb-[40px] pt-[48px]  text-[28px] font-bold leading-[34px]">
+    <div
+      className={`w-full bg-[#E2E8F0] ${poppins.className} flex justify-center pb-6 pt-12`}
+    >
+      <div className="w-[1024px] items-center px-10 pb-[24px] text-center">
+        <h2 className="mb-[40px] text-[28px] font-bold leading-[34px]">
           Speed your creativity and quality
         </h2>
-
-        <div className="mb-[54px] grid items-center gap-[40px] md:grid-cols-2">
-          <div className="flex flex-wrap justify-end ">
-            <Image
-              src={AwardImg1}
-              alt="Clutch Award"
-              className="h-[138px] w-[150px] object-contain"
-            />
-            <div className="flex flex-col items-center">
-              <Image
-                src={AwardImg2}
-                alt="Clutch Award"
-                className="h-[138px] w-[150px] object-contain"
-              />
-              <span className="mt-2 text-center text-gray-600">Awarded</span>
+        <div className="flex items-start gap-10">
+          <div className="flex flex-col items-center">
+            <div className="flex gap-[18px]">
+              <div className="relative h-[150px] w-[139px]">
+                <Image src={AwardImg1} alt="Clutch Award" fill />
+              </div>
+              <div className="relative h-[150px] w-[139px]">
+                <Image src={AwardImg2} alt="Clutch Award" fill />
+              </div>
+              <div className="relative h-[150px] w-[139px]">
+                <Image src={AwardImg3} alt="Clutch Award" fill />
+              </div>
             </div>
-            <Image
-              src={AwardImg3}
-              alt="Clutch Award"
-              className="h-[138px] w-[150px] object-contain"
-            />
+            <span className={`${roboto.className} text-[#636D80]`}>
+              Awarded
+            </span>
           </div>
 
-          <div className="mb-8 items-start  text-start text-[28px] font-normal leading-[33.6px] text-black">
-            All the tools you need to create <br /> social media, videos,
-            <br />
-            speeches...all in one place.
+          <div className="mt-7 text-start text-[28px] font-normal leading-[33.6px] text-black">
+            All the tools you need to create social media, videos, speeches...
+            all in one place.
           </div>
         </div>
       </div>
