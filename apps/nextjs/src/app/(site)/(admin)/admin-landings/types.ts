b1394b26f5c15bf2a1e24236eaba5dfabe172261
AgustinSant <agustinmsant@gmail.com>
2025-01-23 08:28:35 -0300

    update db en site references for free demo LP

--- apps/nextjs/src/app/(site)/(admin)/admin-landings/types.ts
+++ apps/nextjs/src/app/(site)/(admin)/admin-landings/types.ts
@@ -2,7 +2,7 @@ export interface LandingPage {
   id?: string;
   segment: string;
   title: string;
-  lp_type: "regular" | "tools" | "saasy";
+  lp_type: "regular" | "tools" | "free demo";
   description: string;
   sub_description: string;
   video_url: string;
