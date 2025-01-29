9210d40ba02bd79a1523cec7e68114f1181d1f76
martinsosafer <martin@tesserakt.gg>
2025-01-29 13:03:06 -0300

    changes to responsive

--- apps/nextjs/src/app/(site)/masterclasses/masterclasses-landing/coursecards/index.tsx
+++ apps/nextjs/src/app/(site)/masterclasses/masterclasses-landing/coursecards/index.tsx
@@ -6,7 +6,7 @@ import { Button } from "@voiceai/ui";
 import { Card, CardContent } from "@voiceai/ui/@/components/ui/card";
 import { IconChevronRight } from "@voiceai/ui/@/components/ui/icons";
 
-import { poppins } from "~/app/fonts";
+import { poppins, roboto } from "~/app/fonts";
 
 interface Video {
   course: string;
@@ -41,12 +41,16 @@ function CourseCard({
   };
 
   const ContentSection = () => (
-    <div className="flex h-full flex-col justify-between p-0">
-      <div>
-        <h3 className="mb-4 line-clamp-1 text-xl font-semibold text-blue-600">
+    <div className="flex h-full flex-col justify-between p-0 lg:h-[244px] lg:w-[368px] lg:flex-col lg:justify-between">
+      <div className="mx-[24px] lg:mx-0">
+        <h3 className="mb-4  text-[20px] font-bold leading-[28px] text-blue-600 lg:mb-3  lg:text-[24px] lg:leading-[33.6px]">
           {data.course}
         </h3>
-        <p className="mb-6 line-clamp-3 text-gray-600">{data.description}</p>
+        <p
+          className={`mb-3  text-[14px] font-normal leading-[19.6px] text-black ${roboto.className}  lg:text-[16px] lg:leading-[22.4px]`}
+        >
+          {data.description}
+        </p>
       </div>
       <div onClick={handleCourseClick}>
         <Link href={`/masterclasses/${data.id}/1`} target="_blank">
@@ -62,7 +66,7 @@ function CourseCard({
   );
 
   const VideoSection = () => (
-    <div className="aspect-video h-full w-full">
+    <div className="aspect-video px-[20px] lg:h-[254px] lg:w-[450px]">
       <iframe
         src={data.videoUrl}
         className="h-full w-full rounded-lg"
@@ -73,19 +77,15 @@ function CourseCard({
   );
 
   return (
-    <Card className="overflow-hidden rounded-3xl bg-white">
+    <Card className="mx-[24px] h-[480px] w-[355px] overflow-hidden rounded-3xl bg-white lg:h-full lg:w-full">
       <CardContent className="grid gap-8 p-8 lg:grid-cols-2">
-        {isEven ? (
-          <>
-            <ContentSection />
-            <VideoSection />
-          </>
-        ) : (
-          <>
-            <VideoSection />
-            <ContentSection />
-          </>
-        )}
+        {/* Always show VideoSection first on mobile */}
+        <div className={isEven ? "lg:order-2" : "lg:order-1"}>
+          <VideoSection />
+        </div>
+        <div className={isEven ? "lg:order-1" : "lg:order-2"}>
+          <ContentSection />
+        </div>
       </CardContent>
     </Card>
   );
@@ -108,13 +108,15 @@ export default function CourseListing({
     .filter((video): video is Video => video !== undefined);
 
   return (
-    <div className={`bg-cp-primary min-h-[2331px]  ${poppins.className}`}>
+    <div
+      className={`bg-cp-primary h-[3008px] lg:min-h-[2331px]  ${poppins.className}`}
+    >
       <div className="mx-auto max-w-[944px] items-center space-y-8 py-[60px]">
-        <div className=" text-start">
-          <h2 className="text-cp-secondary-lightest  font-bold lg:text-[42px] lg:leading-[50px]  ">
+        <div className=" mx-[24px] text-start">
+          <h2 className="text-cp-secondary-lightest text-[28px] font-bold  leading-[33.6px] lg:text-[42px] lg:leading-[50px]  ">
             These 60+ course modules will take
           </h2>
-          <h2 className="text-cp-secondary-lightest mb-[32px]  font-bold lg:text-[42px] lg:leading-[50px]  ">
+          <h2 className="text-cp-secondary-lightest mb-[32px]  text-[26px] font-bold leading-[31px] lg:text-[42px] lg:leading-[50px]  ">
             beginners through experts to new heights.
           </h2>
           <p className="text-cp-accent text-[34px] font-bold leading-[41px]">
