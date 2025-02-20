20a522c29451d6c6507239c1a0c9b9c96557579c
AgustinSant <agustinmsant@gmail.com>
2025-02-20 08:58:05 -0300

    update credits tables

--- packages/db/schema/imgCredit.ts
+++ packages/db/schema/imgCredit.ts
@@ -7,6 +7,7 @@ export const imgCredit = pgTable("img_credit", {
   id: uuid("id").notNull().primaryKey().defaultRandom(),
   userId: text("user_id")
     .notNull()
+    .unique()
     .references(() => users.id, { onDelete: "cascade" }),
   credits: integer("credits").notNull().default(10),
   created_at: timestamp("created_at").notNull().defaultNow(),
