fa8466d34d3883f9bc32435986192f033359fcb9
Emiliano Sasal <emiliano.sasal@gmail.com>
2025-03-13 21:48:37 -0300

    Image generation using Fal Api with flux-lora model

--- apps/nextjs/src/app/api/generate-image/route.ts
+++ apps/nextjs/src/app/api/generate-image/route.ts
@@ -1,7 +1,5 @@
-import { fal } from "@ai-sdk/fal";
+import { fal } from "@fal-ai/client";
 import { kv } from "@vercel/kv";
-import { experimental_generateImage as generateImage } from "ai";
-import OpenAI from "openai";
 
 import { auth } from "@voiceai/auth";
 import { db, eq, schema, sql } from "@voiceai/db";
@@ -9,12 +7,6 @@ import { db, eq, schema, sql } from "@voiceai/db";
 import { nanoid } from "~/utils/helpers";
 
 export const maxDuration = 60;
-
-// export const runtime = "edge";
-
-const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY!,
-});
 
 export async function POST(req: Request): Promise<Response> {
   const session = await auth();
@@ -25,41 +17,37 @@ export async function POST(req: Request): Promise<Response> {
   try {
     const data = (await req.json()) as { text: string };
 
-    const { image } = await generateImage({
-      model: fal.image("fal-ai/flux-lora"),
-      prompt: "A serene mountain landscape at sunset",
+    // "fal-ai/flux-lora"
+    // "fal-ai/imagen3/fast"
+    const response = await fal.subscribe("fal-ai/flux-lora", {
+      input: {
+        prompt: data.text,
+        image_size: { width: 1792, height: 1024 },
+        num_images: 1,
+      },
+      logs: true,
     });
 
-    console.log("IMAGE", image);
+    const image_url = response?.data?.images[0]?.url ?? "";
 
-    // const response = await openai.images.generate({
-    //   model: "dall-e-3",
-    //   prompt: data.text,
-    //   n: 1,
-    //   size: "1792x1024",
-    //   quality: "hd",
-    // });
+    if (image_url) {
+      await db
+        .update(schema.imgCredit)
+        .set({ credits: sql`${schema.imgCredit.credits} - 1` })
+        .where(eq(schema.imgCredit.userId, session?.user.id));
 
-    // const image_url = response?.data[0]?.url ?? "";
+      const id = nanoid();
+      const createdAt = Date.now();
 
-    // if (image_url) {
-    //   await db
-    //     .update(schema.imgCredit)
-    //     .set({ credits: sql`${schema.imgCredit.credits} - 1` })
-    //     .where(eq(schema.imgCredit.userId, session?.user.id));
+      await kv.hmset(`aiImage:${id}`, { prompt: data.text, image_url });
 
-    //   const id = nanoid();
-    //   const createdAt = Date.now();
+      await kv.zadd(`user:aiImage:${session?.user.id}`, {
+        score: createdAt,
+        member: `aiImage:${id}`,
+      });
+    }
 
-    //   await kv.hmset(`aiImage:${id}`, { prompt: data.text, image_url });
-
-    //   await kv.zadd(`user:aiImage:${session?.user.id}`, {
-    //     score: createdAt,
-    //     member: `aiImage:${id}`,
-    //   });
-    // }
-
-    // return new Response(JSON.stringify(image_url));
+    return new Response(JSON.stringify(image_url));
   } catch (err) {
     console.error(err);
     return new Response((err as Error).message as BodyInit | null | undefined, {
@@ -67,3 +55,66 @@ export async function POST(req: Request): Promise<Response> {
     });
   }
 }
+
+// OpenAi Dall-E code //
+
+// import { kv } from "@vercel/kv";
+// import OpenAI from "openai";
+
+// import { auth } from "@voiceai/auth";
+// import { db, eq, schema, sql } from "@voiceai/db";
+
+// import { nanoid } from "~/utils/helpers";
+
+// export const maxDuration = 60;
+
+// // export const runtime = "edge";
+
+// const openai = new OpenAI({
+//   apiKey: process.env.OPENAI_API_KEY!,
+// });
+
+// export async function POST(req: Request): Promise<Response> {
+//   const session = await auth();
+
+//   if (!session) {
+//     return new Response("Unauthorized", { status: 401 });
+//   }
+//   try {
+//     const data = (await req.json()) as { text: string };
+
+//     const response = await openai.images.generate({
+//       model: "dall-e-3",
+//       prompt: data.text,
+//       n: 1,
+//       size: "1792x1024",
+//       quality: "hd",
+//     });
+
+//     const image_url = response?.data[0]?.url ?? "";
+
+//     if (image_url) {
+//       await db
+//         .update(schema.imgCredit)
+//         .set({ credits: sql`${schema.imgCredit.credits} - 1` })
+//         .where(eq(schema.imgCredit.userId, session?.user.id));
+
+//       const id = nanoid();
+//       const createdAt = Date.now();
+
+//       await kv.hmset(`aiImage:${id}`, { prompt: data.text, image_url });
+
+//       await kv.zadd(`user:aiImage:${session?.user.id}`, {
+//         score: createdAt,
+//         member: `aiImage:${id}`,
+//       });
+//     }
+
+//     return new Response(JSON.stringify(image_url));
+//   } catch (err) {
+//     console.error(err);
+//     return new Response((err as Error).message as BodyInit | null | undefined, {
+//       status: 500,
+//     });
+//   }
+// }
