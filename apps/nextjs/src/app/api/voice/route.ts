615eadf3149a6e377b4a5876ef315ecf7df27a70
emisasal <emiliano.sasal@gmail.com>
2025-04-13 00:47:27 -0300

    Expanding execution time to 120 seconds for /api/voice

--- apps/nextjs/src/app/api/voice/route.ts
+++ apps/nextjs/src/app/api/voice/route.ts
@@ -10,7 +10,7 @@ function addWatermark(message: string) {
   return `${message} -  - ${suffix}`;
 }
 
-export const maxDuration = 90; // This function can run for a maximum of 90 seconds
+export const maxDuration = 120; // This function can run for a maximum of 120 seconds
 
 export async function POST(req: Request) {
   try {
