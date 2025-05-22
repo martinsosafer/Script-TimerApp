d085db8b45f52012eb205a009fe3968d32d9ba16
martinsosafer <martin@tesserakt.gg>
2025-05-22 09:10:38 +0200

    changes to responsive text to voice

--- apps/nextjs/src/app/(site)/components/texttospeech/voicewidget/selectedcard/selectedcard.tsx
+++ apps/nextjs/src/app/(site)/components/texttospeech/voicewidget/selectedcard/selectedcard.tsx
@@ -1,12 +1,11 @@
 import React from "react";
-
 import { IconUserRound } from "@voiceai/ui/@/components/ui/icons";
 
 const SelectedModelCard = ({ selectedModel }) => {
   return (
     <div className="mb-2 cursor-pointer rounded-lg bg-white shadow-sm dark:bg-slate-500">
-      <div className="flex items-center p-1 ">
-        <div className="mr-2  h-7 w-7 flex-shrink-0 overflow-hidden rounded-full">
+      <div className="flex items-center p-1">
+        <div className="mr-2 h-5 w-5 lg:h-7 lg:w-7 flex-shrink-0 overflow-hidden rounded-full">
           {selectedModel ? (
             selectedModel.picture ? (
               <img
@@ -23,12 +22,12 @@ const SelectedModelCard = ({ selectedModel }) => {
         </div>
 
         <div className="flex-grow">
-          <h2 className="text-sm font-semibold dark:text-secondary-foreground">
+          <h2 className="text-xs lg:text-sm font-semibold dark:text-secondary-foreground">
             {selectedModel ? selectedModel.name : "Choose Actor"}
           </h2>
 
           {selectedModel && (
-            <p className="text-xs text-gray-500 dark:text-slate-100">
+            <p className="text-[10px] lg:text-xs text-gray-500 dark:text-slate-100">
               {selectedModel.metadata?.labels.gender ?? "OTHER"}
             </p>
           )}
